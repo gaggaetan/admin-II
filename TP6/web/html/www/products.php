b193e95446ca58f0e4ab8cb8868ff2ec94ef703a
Alocrl <146443967+Alocrl@users.noreply.github.com>
2024-03-24 21:22:13 +0100

    Update products.php

--- TP6/web/html/www/products.php
+++ TP6/web/html/www/products.php
@@ -17,10 +17,10 @@
 <h1>Catalogue WoodyToys</h1>
 
 <?php
-$dbname = 'woodytoys';
-$dbuser = 'root';
-$dbpass = 'mypass';
-$dbhost = 'db';
+$dbname = getenv('MYSQL_DATABASE');
+$dbuser = getenv('MYSQL_USER');
+$dbpass = getenv('MYSQL_PASSWORD');
+$dbhost = getenv('MYSQL_HOST');
 $connect = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to connect to '$dbhost'");
 mysqli_select_db($connect,$dbname) or die("Could not open the database '$dbname'");
 $result = mysqli_query($connect,"SELECT id, name, price FROM products");
